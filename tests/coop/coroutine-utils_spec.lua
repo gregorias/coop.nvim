325e05da122096faa1c8d63bc2437755b2b302e4
Grzegorz Milka <grzegorzmilka@gmail.com>
2024-11-01 15:26:48 +0100

    chore: test copcall some more

--- tests/coop/coroutine-utils_spec.lua
+++ tests/coop/coroutine-utils_spec.lua
@@ -8,18 +8,32 @@ describe("coop.coroutine-utils", function()
 	describe("copcall", function()
 		it("executes a throwing coroutine function in a protected mode", function()
 			local throw_after_sleep = function()
-				uv.sleep(10)
+				uv.sleep(2)
 				error("error", 0)
 			end
 			local f_co = function()
-				local success, err_msg = copcall(throw_after_sleep)
-				return success, err_msg
+				return copcall(throw_after_sleep)
 			end
 
-			local success, err_msg = coop.spawn(f_co):wait(30, 10)
+			local success, err_msg = coop.spawn(f_co):wait(5, 1)
 
 			assert.is.False(success)
 			assert.are.same("error", err_msg)
+		end)
+
+		it("executes a successful coroutine function in a protected mode", function()
+			local throw_after_sleep = function()
+				uv.sleep(2)
+				return "foo"
+			end
+			local f_co = function()
+				return copcall(throw_after_sleep)
+			end
+
+			local success, val = coop.spawn(f_co):wait(5, 1)
+
+			assert.is.True(success)
+			assert.are.same("foo", val)
 		end)
 	end)
 end)
