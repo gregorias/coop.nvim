caefa83b0d861ae21055f1944c64f09ffbd0a95c
Grzegorz Milka <grzegorzmilka@gmail.com>
2024-11-01 10:50:11 +0100

    feat: ensure compatibility with vim.api

--- lua/coop/uv.lua
+++ lua/coop/uv.lua
@@ -5,7 +5,19 @@ local coop = require("coop")
 local functional_utils = require("coop.functional-utils")
 local shift_cb = functional_utils.shift_parameters
 
-local timer_start = coop.cb_to_co(shift_cb(vim.uv.timer_start))
+--- Wraps the callback param with `vim.schedule_wrap`.
+---
+--- This is useful for Libuv functions to ensure that their continuations can run `vim.api` functions without problems.
+---
+---@param f function
+---@return function
+local schedule_cb = function(f)
+	return function(cb, ...)
+		f(vim.schedule_wrap(cb), ...)
+	end
+end
+
+local timer_start = coop.cb_to_co(schedule_cb(shift_cb(vim.uv.timer_start)))
 
 --- Sleeps for a number of milliseconds.
 ---
