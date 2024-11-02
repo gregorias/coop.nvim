52c749ce147899a7978849399374a2f284ebafdc
Grzegorz Milka <grzegorzmilka@gmail.com>
2024-11-02 08:35:01 +0100

    feat: add catchable cancellation

--- lua/coop/task.lua
+++ lua/coop/task.lua
@@ -1,30 +1,40 @@
 --- This module provides a task implementation.
 local M = {}
 
---- A task is a thin wrapper on top of a coroutine.
+--- A task is an extension of Lua coroutine.
 ---
---- A task enhances a coroutine with a future that can be awaited and the ability to capture errors.
+--- A task enhances Lua coroutine with:
 ---
---- For convenience, a task exposes its future’s methods.
+--- - A future that can be awaited.
+--- - The ability to capture errors.
+--- - The ability to cancel and handle cancellation.
 ---
 ---@class Task
 ---@field thread thread the coroutine thread
 ---@field future Future the future for the coroutine
 ---@field cancelled boolean whether the task has been cancelled
+---
+---@field cancel fun(Task) cancels the task
+---@field resume fun(Task, ...) resumes the task
+---@field status fun(Task): string returns the task’s status
+---
 ---@field await function awaits the task
 ---@field await_cb function awaits the task
 ---@field wait function synchronously waits for the task
 
 local running_task = nil
 
+--- Creates a new task.
+---
+---@param tf function the task function
 ---@return Task
-M.create = function(co)
+M.create = function(tf)
 	local future_m = require("coop.future")
 	local future = future_m.Future.new()
 
 	local task = {
 		thread = coroutine.create(function(...)
-			future:complete(co(...))
+			future:complete(tf(...))
 		end),
 		future = future,
 		cancelled = false,
@@ -32,6 +42,10 @@ M.create = function(co)
 
 	return setmetatable(task, {
 		__index = {
+			cancel = M.cancel,
+			resume = M.resume,
+			status = M.status,
+
 			await = function(self, ...)
 				return self.future:await(...)
 			end,
@@ -53,10 +67,10 @@ end
 ---@param task Task the task to resume
 ---@param ... ... the arguments
 ---@return boolean success
----@return ...
+---@return any ... results
 M.resume = function(task, ...)
-	if task.cancelled then
-		return false, "The task was cancelled."
+	if task:status() ~= "suspended" then
+		error("Tried to resume a task that is not suspended but " .. task:status() .. ".", 0)
 	end
 
 	local previous_task = M.running()
@@ -72,6 +86,8 @@ M.resume = function(task, ...)
 end
 
 --- Cancels a task.
+---
+---@param task Task the task to cancel
 M.cancel = function(task)
 	local task_status = M.status(task)
 
@@ -84,8 +100,10 @@ M.cancel = function(task)
 		error("You cannot cancel the currently running task.", 0)
 	end
 
+	assert(task_status == "suspended")
+
 	task.cancelled = true
-	task.future:set_error("Task was cancelled.")
+	task:resume()
 end
 
 --- Returns the currently running task or nil.
@@ -96,16 +114,34 @@ M.running = function()
 end
 
 --- Yields the current task.
-M.yield = coroutine.yield
+---
+--- This is a coroutine function.
+---
+--- If inside a cancelled task, it throws an error message "cancelled".
+---
+---@async
+---@return any ... the arguments passed to task.resume
+M.yield = function()
+	local args = { coroutine.yield() }
+
+	local this = M.running()
+
+	if not this then
+		error("coroutine.yield returned without a running task. Make sure that you use task.resume to resume tasks.", 0)
+	end
+
+	if this.cancelled then
+		error("cancelled", 0)
+	end
+
+	return unpack(args)
+end
 
 --- Returns the status of a task’s thread.
 ---
 ---@param task Task the task
 ---@return string "running" | "suspended" | "normal" | "dead"
 M.status = function(task)
-	if task.cancelled then
-		return "dead"
-	end
 	return coroutine.status(task.thread)
 end
 
--- tests/coop/task_spec.lua
+++ tests/coop/task_spec.lua
@@ -1,11 +1,12 @@
 --- Busted tests for coop.task.
 local coop = require("coop")
+local copcall = require("coop.coroutine-utils").copcall
 local task = require("coop.task")
 local uv = require("coop.uv")
 
 describe("coop.task", function()
 	describe("cancel", function()
-		it("cancelled task’s future returns an error and it never executes", function()
+		it("cancelled task’s future returns an error and it becomes dead", function()
 			local done = false
 			local spawned_task = coop.spawn(function()
 				uv.sleep(20)
@@ -13,21 +14,33 @@ describe("coop.task", function()
 			end)
 
 			-- Immediately cancel the task.
-			task.cancel(spawned_task)
+			spawned_task:cancel()
 
 			-- Let the timer run out.
 			vim.wait(40)
 
+			-- Test that the task didn’t finish.
 			assert.is.False(done)
-			assert.are.same({ false, "The task was cancelled." }, { task.resume(spawned_task) })
-			assert.are.same("dead", task.status(spawned_task))
+			assert.are.same("dead", spawned_task:status())
+		end)
+
+		it("can be captured in yield", function()
+			local success, err_msg = true, ""
+			local spawned_task = coop.spawn(function()
+				success, err_msg = copcall(uv.sleep, 20)
+				error(err_msg)
+			end)
+
+			-- Immediately cancel the task.
+			spawned_task:cancel()
+
+			assert.is.False(success)
+			assert.are.same("cancelled", err_msg)
 		end)
 
 		it("doesn’t do anything on finished tasks", function()
 			local spawned_task = coop.spawn(function() end)
-
-			task.cancel(spawned_task)
-
+			spawned_task:cancel()
 			assert.is.False(spawned_task.cancelled)
 		end)
 
@@ -41,5 +54,30 @@ describe("coop.task", function()
 			assert.is.False(success)
 			assert.are.same("You cannot cancel the currently running task.", err)
 		end)
+	end)
+
+	describe("resume", function()
+		it("throws an error on dead tasks", function()
+			local spawned_task = coop.spawn(function() end)
+			local success, err_msg = pcall(task.resume, spawned_task)
+
+			assert.is.False(success)
+			assert.are.same("Tried to resume a task that is not suspended but dead.", err_msg)
+		end)
+	end)
+
+	describe("yield", function ()
+		it("throws if used outside of a task", function()
+			local t = task.create(function ()
+				task.yield()
+			end)
+
+			coroutine.resume(t.thread)
+			local success, err_msg = coroutine.resume(t.thread)
+
+			assert.is.False(success)
+			assert.are.same("coroutine.yield returned without a running task. Make sure that you use task.resume to resume tasks.", err_msg)
+		end)
+
 	end)
 end)
