68fd4ae76ca9034746c4642464bc6721d0eac1e8
Grzegorz Milka <grzegorzmilka@gmail.com>
2024-11-08 11:11:20 +0100

    refactor(uv): clean up

--- lua/coop/uv.lua
+++ lua/coop/uv.lua
@@ -2,41 +2,48 @@
 local M = {}
 
 local coop = require("coop")
-local functional_utils = require("coop.functional-utils")
-local shift_cb = functional_utils.shift_parameters
 
 --- Wraps the callback param with `vim.schedule_wrap`.
 ---
 --- This is useful for Libuv functions to ensure that their continuations can run `vim.api` functions without problems.
 ---
 ---@param f function
----@param cb_pos? number the position of the callback parameter
+---@param cb_pos? number|string the position of the callback parameter
 ---@return function
 local schedule_cb = function(f, cb_pos)
-	cb_pos = cb_pos or 1
+	cb_pos = cb_pos or "last"
 
 	return function(cb, ...)
 		local pack = require("coop.table-utils").pack
 		local unpack = require("coop.table-utils").unpack_packed
 		local safe_insert = require("coop.table-utils").safe_insert
 		local args = pack(...)
+		if cb_pos == "last" then
+			cb_pos = select("#", ...) + 1
+		end
+		---@diagnostic disable-next-line: param-type-mismatch
 		safe_insert(args, cb_pos, args.n, vim.schedule_wrap(cb))
 		args.n = args.n + 1
 		f(unpack(args))
 	end
 end
 
+--- Wraps a Libuv function into a task function.
+---
+---@param f function
+---@param cb_pos? number|string the position of the callback parameter
+---@return function tf
 local wrap = function(f, cb_pos)
 	return coop.cb_to_tf(schedule_cb(f, cb_pos))
 end
 
-M.timer_start = coop.cb_to_tf(schedule_cb(shift_cb(vim.uv.timer_start)))
-M.fs_open = wrap(vim.uv.fs_open, 4)
-M.fs_close = wrap(vim.uv.fs_close, 2)
-M.fs_fstat = wrap(vim.uv.fs_fstat, 2)
+M.timer_start = wrap(vim.uv.timer_start)
+M.fs_open = wrap(vim.uv.fs_open)
+M.fs_close = wrap(vim.uv.fs_close)
+M.fs_fstat = wrap(vim.uv.fs_fstat)
 M.fs_opendir = wrap(vim.uv.fs_opendir, 2)
-M.fs_readdir = wrap(vim.uv.fs_readdir, 2)
-M.fs_closedir = wrap(vim.uv.fs_closedir, 2)
+M.fs_readdir = wrap(vim.uv.fs_readdir)
+M.fs_closedir = wrap(vim.uv.fs_closedir)
 
 --- Sleeps for a number of milliseconds.
 ---
