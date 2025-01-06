913cc0cd20927bdc09fcd1f9cfc71134e4a8e9a3
Stefan Boca <stefan.r.boca@gmail.com>
2025-01-05 22:40:50 -0800

    fix: use builtin neovim runtime types

--- lua/coop/vim.lua
+++ lua/coop/vim.lua
@@ -1,23 +1,6 @@
 --- This module provides a task function version of vim.system.
 local M = {}
 
----@class vim.SystemOpts
----@field cwd? string
----@field env? table<string,string>
----@field clear_env? boolean
----@field stdin? string|string[]|boolean
----@field stdout? boolean|function
----@field stderr? boolean|function
----@field text? boolean
----@field timeout? integer
----@field detach? boolean
-
----@class vim.SystemCompleted
----@field code integer
----@field signal integer
----@field stdout string
----@field stderr string
-
 --- Runs a system command or throws an error if cmd cannot be run.
 ---
 ---@async
